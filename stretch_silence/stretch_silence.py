b0ae514f1afeae4e20a87a522a13b1faf65fe5fd
Gwendolyn Harris <scrupulous.imp@gmail.com>
2023-06-09 12:11:00 -0400

    Begin initial debugging and tests

--- stretch_silence/stretch_silence.py
+++ stretch_silence/stretch_silence.py
@@ -9,8 +9,8 @@ import os
 
 def change_speed(input: str, output: str, type: str, speed: int=1.0):
     # Saves output temporarily in the user-defined output location for further changes, if necessary
-    if os.path.splittext(input)[1] != ".wav":
-        filetype = os.path.splittext(input)[1]
+    if os.path.splitext(input)[1] != ".wav":
+        filetype = os.path.splitext(input)[1]
         sound = AudioSegment.from_file(input)
         sound.export(output, format="wav")
         input = output
@@ -36,7 +36,7 @@ def add_space(input: str, space: int, bounds: int) -> AudioSegment:
     return clip_with_space
 
 def get_presets(name: str):
-    with open('presets.toml', 'r') as f:
+    with open('presets.toml', 'rb') as f:
         presets = tomllib.load(f)
     params = presets[name]
 
@@ -47,8 +47,8 @@ def main():
         prog='Stretch and Add Silence',
         description='A little CLI widget intended to slow and add space to various forms of language-learning audio files, which often feel rushed to newcomers. Thank you to the creators of PyDub and AudioTSM, upon which this program sits.',
         epilog='Happy learning!')
-    parser.add_argument('infile', type=argparse.FileType('r'), help="The sound file to be processed. Accepts any ffmpeg filetype.")
-    parser.add_argument('-d', '--outfile', type=argparse.FileType('w'), help="Where you want the processed audio to be saved. Defaults to the same location as your input file.")
+    parser.add_argument('infile', type=str, help="The sound file to be processed. Accepts any ffmpeg filetype.")
+    parser.add_argument('-d', '--outfile', type=str, help="Where you want the processed audio to be saved. Defaults to the same location as your input file.")
     parser.add_argument('-s', '--speed', type=float, help="Speed for the new audio, relative to the original.")
     parser.add_argument('-t', '--type', type=str, help="The filetype you wish to the save your processed audio in. Accepts any ffmpeg filetype and defaults to the same type as the input.")
     parser.add_argument('-e', '--emptyspace', type=int, help='Amount of empty space you would like between sounds, in milliseconds.')
@@ -57,23 +57,27 @@ def main():
 
     args = parser.parse_args()
     my_args = vars(args)
+    print(my_args)
 
-    if "outfile" not in my_args:
-        my_args["outfile"] = os.path.dirname(my_args["infile"])
+    if my_args["type"] == None:
+        my_args["type"] = os.path.splitext(my_args["infile"])[1][1::]
 
-    if "type" not in my_args:
-        my_args["type"] = os.path.splittext(my_args["infile"])[1]
+    if my_args["outfile"] == None:
+        my_args["outfile"] = os.path.splitext(my_args["infile"])[0] + "(output)." + my_args["type"]
+        print(my_args["outfile"])
 
-    if "preset" in my_args:
+    if my_args["preset"]:
         params = get_presets(my_args["preset"])
         for k, v in params:
             my_args[k] = v
 
-    if "speed" in my_args:
+    print(my_args)
+
+    if my_args["speed"]:
         change_speed(my_args["infile"], my_args["outfile"], my_args["speed"])
         my_args["infile"] = my_args["outfile"]
 
-    if "emptyspace" in my_args:
+    if my_args["emptyspace"]:
         output = add_space(my_args["infile"], my_args["emptyspace"], my_args["boundthreshold"])
         output.export(my_args["outfile"], format=my_args["type"])
 
